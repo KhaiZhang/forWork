6df1873f78e1b852cccaa38a61c5ab3341923d09
Khai <KhaiZhang@163.com>
2019-06-26 22:51:51 +0800

    6.26

---
+++ mul/index.js
@@ -0,0 +1,10 @@
+(function(){
+	 var i , j;
+            for(i = 1;i <= 9;i ++){
+                document.write("<br>");
+                for(j = 1;j <= i;j ++){
+                    sum = i * j;
+                    document.write(j ,"*",i ,"=",sum,"   "); 
+                }
+            }
+}());
